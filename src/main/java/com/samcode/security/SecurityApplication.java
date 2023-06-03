6c0be4841e27f556f88bece1555d772ac2d10a39
sam-sli <ismailslitini98@gmail.com>
2023-06-03 19:35:56 +0100

    implements Refresh token Logout Mecanisem and Roles

--- src/main/java/com/samcode/security/SecurityApplication.java
+++ src/main/java/com/samcode/security/SecurityApplication.java
@@ -1,7 +1,15 @@
 package com.samcode.security;
 
+import com.samcode.security.auth.AuthenticationService;
+import com.samcode.security.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.samcode.security.user.Role.ADMIN;
+import static com.samcode.security.user.Role.MANAGER;
+
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -10,4 +18,29 @@ public class SecurityApplication {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
 }
